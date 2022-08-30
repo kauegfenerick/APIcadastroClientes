334466f92c1419eed9080b5a4294753fb302bcdd
Kaue Senac <kauegomesfenerick@gmail.com>
2022-08-30 20:05:48 -0300

    Model Cliente e método Idade

---
+++ CadastroClientes/Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroClientes.Models
+{
+    public class Cliente
+    {
+        public Cliente(string nome, DateTime nascimento, string email)
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Email = email;
+        }
+
+        public int Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public DateTime Nascimento { get; private set; }
+
+        public string Email { get; private set; }
+
+        public int Idade()
+        {
+            int idade = DateTime.Now.Year - Nascimento.Year;
+
+            if (DateTime.Now.DayOfYear < Nascimento.DayOfYear)
+                idade--;
+
+            return idade;
+        }
+    }
+}
