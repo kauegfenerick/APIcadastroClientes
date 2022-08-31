469dfb3141e722b29545c55b0cfa895f88562170
Kaue Senac <kauegomesfenerick@gmail.com>
2022-08-30 21:25:35 -0300

    Criação do Db

--- CadastroClientes/Startup.cs
+++ CadastroClientes/Startup.cs
@@ -1,7 +1,9 @@
+using CadastroClientes.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,12 +28,15 @@ namespace CadastroClientes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LocalDb")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CadastroClientes", Version = "v1" });
+                
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cadastro de Clientes", Version = "v1.0" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
