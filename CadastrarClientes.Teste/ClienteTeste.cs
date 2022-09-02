120b07dbe821443e00ab8e92d72e44eec5ad2a5c
KaueSenac <kauegomesfenerick@gmail.com>
2022-09-01 21:58:13 -0300

    Primeiro teste

---
+++ CadastrarClientes.Teste/ClienteTeste.cs
@@ -0,0 +1,22 @@
+﻿using CadastroClientes.Models;
+using System;
+using Xunit;
+
+namespace CadastroClientes.Teste
+{
+    public class ClienteTest
+    {
+        [Fact]
+        public void Idade_20anosDepois_retornar20()
+        {
+            //arrange
+            Cliente cliente = new Cliente("Joalisson", DateTime.Now.AddYears(-20), "joauau@mail.com");
+
+            //act
+            var idade = cliente.Idade();
+
+            //assert
+            Assert.Equal(20, idade);
+        }
+    }
+}
